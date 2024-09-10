f201b782a9b6a46a032c7987009936d3ecd3b66b
David Alejandro Cajiao Lazt <123507601+DCajiao@users.noreply.github.com>
2024-09-10 09:35:25 -0500

    fix: response status code

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ app = Flask(__name__)
 def post():
     data = request.json
     logging.info(data)
-    return 'Data saved'
+    return 'Data saved', 200
 
 
 @app.route('/get', methods=['GET'])
 def get_logs():
     with open('logs/data.txt', 'r') as file:
         data = file.read()
-    return data
+    return data, 200
 
 @app.route('/delete', methods=['DELETE'])
 def delete_logs():
     with open('logs/data.txt', 'w') as file:
         file.write('')
-    return 'Logs deleted'
+    return 'Logs deleted', 200
 
 if __name__ == '__main__':
     if not os.path.exists('logs'):
